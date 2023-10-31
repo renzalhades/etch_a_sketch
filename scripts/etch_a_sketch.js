c535a891083be4677b2cab3fec8233e2e9e6c634
renzalhades <renzalhades@gmail.com>
2023-11-01 04:01:03 +0800

    Initial implementation of features like sketch, grid clearing, grid size and color setting .

--- scripts/etch_a_sketch.js
+++ scripts/etch_a_sketch.js
@@ -1,17 +1,54 @@
 
 
-function gridSize (size) {
+let color = 'black';
+let INITIAL_SIZE = 30;
+
+
+
+setGridSize (12);
+
+const colorPicker = document.querySelector ('.color-picker');
+const gridSize = document.querySelector ('.grid-size-setting');
+const eraser = document.querySelector ('.eraser-setting');
+const clean = document.querySelector ('button.clean-setting');
+
+clean.onclick = () => cleanGrid ();
+colorPicker.oninput = (e) => color = e.target.value;
+gridSize.oninput = (e) => size = e.target.value;
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function setGridSize (size) {
     const gridContainer = document.querySelector ('.board');
     gridContainer.style.setProperty ('--size', size);
-
-    size = size*size;
-    for (let x=1; x <size+1; x++) {
+    let area = size*size;
+    for (let x=1; x <area+1; x++) {
         gridCreate = document.createElement ('div');
-        gridCreate.classList.add ('grid', 'element' + x);
+        gridCreate.setAttribute ('id', ''+x)
         gridContainer.appendChild (gridCreate);
+        gridCreate.style.backgroundColor ='white';
+        gridCreate.addEventListener ('mouseover', paint);
+        gridCreate.addEventListener ('mousedown', paint);
     }
-
 }
 
-gridSize(2)
+function paint (e) {
+
+    if (e.type === 'mouseover' && mouseDown) {
+        e.target.style.backgroundColor = color;
+    }
+    
+}
+
+function cleanGrid () {
+    const gridContainer = document.querySelector ('.board');
+    gridContainer.innerHTML = '';
+    setGridSize (size);
+    // 
+}
+
+
+
 
