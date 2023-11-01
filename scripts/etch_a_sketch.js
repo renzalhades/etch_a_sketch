cf2cb71b899b053c7012f558c8c4ddc15e30c001
renzalhades <renzalhades@gmail.com>
2023-11-01 22:08:39 +0800

    Implemented realtime change of grid color and size when values are adjusted.

--- scripts/etch_a_sketch.js
+++ scripts/etch_a_sketch.js
@@ -1,54 +1,68 @@
 
 
-let color = 'black';
-let INITIAL_SIZE = 30;
+let brushColor = 'black';
+let size = 10;
 
-
-
-setGridSize (12);
-
-const colorPicker = document.querySelector ('.color-picker');
+const brushColorSelect = document.querySelector ('.brush');
+const padColorSelect = document.querySelector ('.pad');
 const gridSize = document.querySelector ('.grid-size-setting');
 const eraser = document.querySelector ('.eraser-setting');
 const clean = document.querySelector ('button.clean-setting');
+const gridSizeValue = document.querySelector ('.grid-size-value');
+const gridContainer = document.querySelector ('.board');
+const gridContainers = document.querySelectorAll ('.board');
+
 
 clean.onclick = () => cleanGrid ();
-colorPicker.oninput = (e) => color = e.target.value;
-gridSize.oninput = (e) => size = e.target.value;
+gridSize.addEventListener ('input', updateGridSize )
+padColorSelect.addEventListener ('input', updateGridColor);
+brushColorSelect.oninput = (e) => brushColor = e.target.value;
+gridContainers.forEach ((grid) => grid.addEventListener ('mouseover', paint))
+gridContainers.forEach ((grid) => grid.addEventListener ('mousedown', paint))
 
 let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+document.body.addEventListener ('mousedown', () => mouseDown = true);
+document.body.addEventListener ('mouseup',() => mouseDown = false );
+
+
 
 function setGridSize (size) {
-    const gridContainer = document.querySelector ('.board');
     gridContainer.style.setProperty ('--size', size);
     let area = size*size;
+    gridSizeValue.innerHTML = size + ' x ' + size;
+
     for (let x=1; x <area+1; x++) {
         gridCreate = document.createElement ('div');
-        gridCreate.setAttribute ('id', ''+x)
+        gridCreate.classList.add ('grid');
         gridContainer.appendChild (gridCreate);
-        gridCreate.style.backgroundColor ='white';
-        gridCreate.addEventListener ('mouseover', paint);
-        gridCreate.addEventListener ('mousedown', paint);
     }
 }
 
 function paint (e) {
+    if (e.type === 'mouseover' && mouseDown) { 
 
-    if (e.type === 'mouseover' && mouseDown) {
-        e.target.style.backgroundColor = color;
-    }
-    
+        e.target.style.backgroundColor = brushColor;
+    }  
 }
 
 function cleanGrid () {
     const gridContainer = document.querySelector ('.board');
     gridContainer.innerHTML = '';
     setGridSize (size);
-    // 
 }
 
+function updateGridSize (e) {
+    size = e.target.value;
+    gridSizeValue.innerHTML = size + ' x ' + size;
+    cleanGrid ();
+}
+
+function updateGridColor (e) {
+    padColor = e.target.value;
+    gridContainer.style.setProperty ('--padColor', padColor);
+}
+
+setGridSize (size);
 
 
 
