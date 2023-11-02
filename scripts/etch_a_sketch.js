6e47ffeffbbf860ef08ce848b32af00bb8b02502
renzalhades <renzalhades@gmail.com>
2023-11-02 12:46:11 +0800

    Implementation of rainbow color and eraser. Made minor modifications.

--- scripts/etch_a_sketch.js
+++ scripts/etch_a_sketch.js
@@ -1,30 +1,31 @@
-
-
+// INITIALIZE VALUES
 let brushColor = 'black';
+let padColor = 'white';
+let mouseDown = false;
 let size = 10;
+let rainbowCounter =0;
 
+const rainbowColor =['red', 'orange', 'yellow', 'green', 'blue', 'violet']
 const brushColorSelect = document.querySelector ('.brush');
 const padColorSelect = document.querySelector ('.pad');
 const gridSize = document.querySelector ('.grid-size-setting');
 const eraser = document.querySelector ('.eraser-setting');
+const rainbow = document.querySelector ('.rainbow-setting');
 const clean = document.querySelector ('button.clean-setting');
 const gridSizeValue = document.querySelector ('.grid-size-value');
 const gridContainer = document.querySelector ('.board');
 const gridContainers = document.querySelectorAll ('.board');
 
-
-clean.onclick = () => cleanGrid ();
-gridSize.addEventListener ('input', updateGridSize )
+clean.addEventListener ('click', cleanGrid);
+gridSize.addEventListener ('input', updateGridSize );
 padColorSelect.addEventListener ('input', updateGridColor);
-brushColorSelect.oninput = (e) => brushColor = e.target.value;
-gridContainers.forEach ((grid) => grid.addEventListener ('mouseover', paint))
-gridContainers.forEach ((grid) => grid.addEventListener ('mousedown', paint))
-
-let mouseDown = false;
+brushColorSelect.addEventListener ('input', (e) => brushColor = e.target.value)
+gridContainers.forEach ((gridBox) => gridBox.addEventListener ('mouseover', paint))
+gridContainers.forEach ((gridBox) => gridBox.addEventListener ('mousedown', paint))
+eraser.addEventListener ('click', toggleMode);
+rainbow.addEventListener('click', toggleMode);
 document.body.addEventListener ('mousedown', () => mouseDown = true);
 document.body.addEventListener ('mouseup',() => mouseDown = false );
-
-
 
 function setGridSize (size) {
     gridContainer.style.setProperty ('--size', size);
@@ -39,9 +40,25 @@ function setGridSize (size) {
 }
 
 function paint (e) {
+    let paint = '';
     if (e.type === 'mouseover' && mouseDown) { 
-
-        e.target.style.backgroundColor = brushColor;
+        switch (true) {
+            case (rainbow.classList.contains('active')): {
+                paint = rainbowColor [rainbowCounter%8];
+                rainbowCounter++
+                break;
+            }
+            case (eraser.classList.contains('active')): {
+                paint= padColor;
+                break;
+            }
+            default: {
+                paint = brushColor;
+                break;
+            }
+        }
+        e.target.style.backgroundColor = paint;
+        console.log (paint);
     }  
 }
 
@@ -60,9 +77,28 @@ function updateGridSize (e) {
 function updateGridColor (e) {
     padColor = e.target.value;
     gridContainer.style.setProperty ('--padColor', padColor);
+    cleanGrid ()
+    return padColor;
+    
 }
 
-setGridSize (size);
+function toggleMode (e) {
+    console.log (e.target.classList.value)
+   
+       if (e.target.classList.value == "eraser-setting" ||
+           e.target.classList.value == "eraser-setting active") {
+            rainbow.classList.remove ('active');
+            eraser.classList.toggle ('active'); 
+        }
+    
+        else if  (e.target.classList.value == "rainbow-setting" || 
+                  e.target.classList.value == "rainbow-setting active") {
+            eraser.classList.remove ('active');
+            rainbow.classList.toggle ('active');
+        }
+}
+    
 
+setGridSize (size);
 
 
